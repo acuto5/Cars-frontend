c9bf088d0dcac4c809ebb48369f189aaaeb751d8
paulius5 <piligrimaz@gmail.com>
2018-11-08 18:42:07 +0200

    Added cars table.

---
+++ src/components/Car.vue
@@ -0,0 +1,23 @@
+<template>
+    <tr>
+        <td>
+            <img src="https://via.placeholder.com/250x125/d2d2d2/?text=no%20image%20yet" onerror="this.src='https://via.placeholder.com/250x125/d2d2d2/?text=no%20image%20yet';" :alt="this.car.brand+' '+this.car.brand" :title="this.car.brand+' '+this.car.brand" class="carImg" />
+        </td>
+
+        <td>{{car._id}}</td>
+        <td>{{car.brand}}</td>
+        <td>{{car.model}}</td>
+
+        <td>{{car.engine}}</td>
+        <td>{{car.price}}</td>
+
+        <td></td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        props:['car'],
+        name: 'car',
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{test}}
+    <section id="intro">
+      <div class="overVideo">
+      </div>
+      <video autoplay muted loop id="carsVideo">
+
+        Your browser does not support HTML5 video.
+      </video>
+    </section>
+
+    <section id="results" class="pt-5">
+      <div class="container">
+        <h1>Search car</h1>
+        <div class="row">
+          <div class="col-12">
+            <table class="table">
+              <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">Id</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Model</th>
+                <th scope="col">Engine</th>
+                <th scope="col">Price</th>
+                <th scope="col">Action</th>
+              </tr>
+              </thead>
+              <tbody id="tbody_cars">
+              <car v-for="c in cars" :car="c" :key="c._id"></car>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import car from '@/components/Car.vue'
 
 export default {
-    cars: [1, 2, 3],
+    components: {
+        car
+    },
     data() {
         return {
-            test: "testas"
+            test: "testas",
+            cars: [],
+
         }
     },created(){
-        console.log(this.cars)
+        fetch("http://localhost:5001/api/cars").then(response => response.json()).then(json=>{
+            console.log(json);
+            this.cars = json;
+        })
     },
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
